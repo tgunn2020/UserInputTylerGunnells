84888fd2a5b8bfc6286e351971e9dcd8d0a51cd2
Tyler G <tyler@webverismo.com>
2020-01-31 17:45:41 -0500

    Final Code Responding to User Input

--- Program.cs
+++ Program.cs
@@ -6,7 +6,38 @@ namespace UserInputTylerGunnells
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hi, this is Tyler Gunnells!");
+            Console.WriteLine("Hi, this is Tyler Gunnells! \n");
+
+            Console.WriteLine("Please type your name in the space below and hit enter.\n");
+
+            string nameresponse = (Console.ReadLine());
+
+            Console.WriteLine("\n" + nameresponse+ ",");
+
+            Console.WriteLine("Please type your question and hit enter.\n");
+
+            string response = (Console.ReadLine());
+
+            Console.WriteLine("\n" + "You responded, " + response);
+
+            Console.WriteLine("\n" + "If this response is correct type YES, if not, type NO." + "\n");
+
+            string confirmation = (Console.ReadLine());
+            {
+                if (confirmation == "YES")
+                {
+                    Console.WriteLine("\n" + "OutPut Confirmation" + "\n" + "Name:" + nameresponse + "\n" + "Question:" + response);
+
+                    Console.WriteLine("\n" + "We will be responding to your invoice shortly.");
+                }
+                else if (confirmation == "NO")
+                {
+                    Console.WriteLine("\n" + "Please restart form from beginning");
+                }
+      
+            }
+
+
         }
     }
 }
