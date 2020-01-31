a3b3b212b8bbd5a86bd934f01e45ff0e64f9587b
Tyler G <tyler@webverismo.com>
2020-01-31 15:26:33 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UserInputTylerGunnells
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi, this is Tyler Gunnells!");
+        }
+    }
+}
